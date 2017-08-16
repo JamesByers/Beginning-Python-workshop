c54a61c450a592ceb25fd7984945fe7d0b196e29
Jim Byers <james.byers.business@gmail.com>
2017-08-16 14:28:13 -0700

    Create Data_types.py

---
+++ 3_Data_types/Data_types.py
@@ -0,0 +1,22 @@
+'''
+Python data types
+'''
+
+'''
+Multi-line comments go between 3 quotation marks.
+You can use single or double quotes.
+'''
+
+# One-line comments are preceded by the pound symbol
+
+
+# BASIC DATA TYPES
+
+x = 5               # creates an object
+print type(x)       # check the type: int (not declared explicitly)
+type(x)             # automatically prints
+type(5)             # assigning it to a variable is not required
+
+type(5.0)           # float
+type('five')        # str
+type(True)          # bool

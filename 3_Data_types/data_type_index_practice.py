31049863f0140f5b625d1094901dcdab8059012d
Jim Byers <james.byers.business@gmail.com>
2017-08-17 17:12:13 -0700

    Create data_type_index_practice.py

---
+++ 3_Data_types/data_type_index_practice.py
@@ -0,0 +1,21 @@
+### Create code to do the following.
+
+# Create an `int` called `my_int` and set it equal to `5`.
+		
+# Add `4` to `my_int`.
+
+# Create a `float` called `my_float` and set it to `3.2`.
+
+# Create a `string` called `my_string` and set it to `'hello'`.
+
+# Create a `boolean` called `my_bool` and set it equal to `True`.
+
+# Add `my_bool` to `my_int`.
+
+# Add `my_bool` to `my_float`.
+
+# Change `my_bool` to `False` and add it to `my_float`.
+
+# Add `my_int` to `my_float`.
+
+# Add `my_int` to `my_string`. Does it work? If not, get it to work.

44b9ebc61c37596c4cb4ea1f15ef4a36faec8d53
Jim Byers <james.byers.business@gmail.com>
2017-08-17 21:16:17 -0700

    Create containers_practice.py

---
+++ 4_Containers/containers_practice.py
@@ -0,0 +1,22 @@
+### Create code to accomplish the following:
+
+# 1. Create a `list` called `my_list` and store the values `3,20,"hello","goodbye"` within it.
+
+
+#2. Print the list in reverse.
+
+
+# 3. Add `4.3` to the list.
+
+
+# 4. Remove `3` from the list.
+
+
+# 5. Create a dictionary called `week_dict` that stores the days of the week as keys and whether or not they are part of the weekend as values.
+
+
+# 6. Collect all of the weekdays into a list.
+
+
+#7. Delete `Monday` from `week_dict`.
+

dd0e5e98cbdd3d8faea8c8ddd1c7e72cc71fa44c
Jim Byers <james.byers.business@gmail.com>
2017-08-17 17:19:27 -0700

    Update solution_for_data_type_index_practice.py

--- 3_Data_types/Solution/solution_for_data_type_index_practice.py
+++ 3_Data_types/Solution/solution_for_data_type_index_practice.py
@@ -1,43 +1,43 @@
 ##  This file contains solutions to the tasks in data_type_index_practice.py
 
-1. Create an `int` called `my_int` and set it equal to `5`.
+# 1. Create an `int` called `my_int` and set it equal to `5`.
 		
 		my_int = 5
     
-2. Add `4` to `my_int`.
+# 2. Add `4` to `my_int`.
 
 		my_int += 4
     
-3. Create a `float` called `my_float` and set it to `3.2`.
+# 3. Create a `float` called `my_float` and set it to `3.2`.
 
 		my_float = 3.2
     
-4. Create a `string` called `my_string` and set it to `'hello'`.
+# 4. Create a `string` called `my_string` and set it to `'hello'`.
 
 		my_string = 'hello'
     
-5. Create a `boolean` called `my_bool` and set it equal to `True`.
+# 5. Create a `boolean` called `my_bool` and set it equal to `True`.
 
 		my_bool = True
     
-6. Add `my_bool` to `my_int`.
+# 6. Add `my_bool` to `my_int`.
 
 		print(my_bool+my_int)
     
-7. Add `my_bool` to `my_float`.
+# 7. Add `my_bool` to `my_float`.
 
 		print(my_bool+my_float)
     
-8. Change `my_bool` to `False` and add it to `my_float`.
+# 8. Change `my_bool` to `False` and add it to `my_float`.
 
 		my_bool = False
 		print(my_bool+my_float)
 
-9. Add `my_int` to `my_float`.
+# 9. Add `my_int` to `my_float`.
 
 		print(my_int+my_float)
     
-10. Add `my_int` to `my_string`. Does it work? If not, get it to work.
+# 10. Add `my_int` to `my_string`. Does it work? If not, get it to work.
 
 		print(my_int+my_string)
 		print(str(my_int)+my_string)

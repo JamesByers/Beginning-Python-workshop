76469732fe5bf44db6f7d7c2075cc5a1c94ec43d
Jim Byers <james.byers.business@gmail.com>
2017-08-17 17:14:44 -0700

    Update data_type_index_practice.py

--- 3_Data_types/data_type_index_practice.py
+++ 3_Data_types/data_type_index_practice.py
@@ -1,21 +1,31 @@
 ### Create code to do the following.
 
-# Create an `int` called `my_int` and set it equal to `5`.
-		
-# Add `4` to `my_int`.
+# 1. Create an `int` called `my_int` and set it equal to `5`.
 
-# Create a `float` called `my_float` and set it to `3.2`.
 
-# Create a `string` called `my_string` and set it to `'hello'`.
+# 2. Add `4` to `my_int`.
 
-# Create a `boolean` called `my_bool` and set it equal to `True`.
 
-# Add `my_bool` to `my_int`.
+# 3. Create a `float` called `my_float` and set it to `3.2`.
 
-# Add `my_bool` to `my_float`.
 
-# Change `my_bool` to `False` and add it to `my_float`.
+# 4. Create a `string` called `my_string` and set it to `'hello'`.
 
-# Add `my_int` to `my_float`.
 
-# Add `my_int` to `my_string`. Does it work? If not, get it to work.
+# 5. Create a `boolean` called `my_bool` and set it equal to `True`.
+
+
+# 6. Add `my_bool` to `my_int`.
+
+
+# 7. Add `my_bool` to `my_float`.
+
+
+# 8. Change `my_bool` to `False` and add it to `my_float`.
+
+
+# 9.  Add `my_int` to `my_float`.
+
+
+# 10. Add `my_int` to `my_string`. Does it work? If not, get it to work.
+

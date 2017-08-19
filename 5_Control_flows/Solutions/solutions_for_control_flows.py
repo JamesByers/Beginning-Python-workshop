aee9ca207a456a078a39a38445c4489dfd6c0034
Jim Byers <james.byers.business@gmail.com>
2017-08-18 17:26:07 -0700

    Create solutions_for_control_flows.py

---
+++ 5_Control_flows/Solutions/solutions_for_control_flows.py
@@ -0,0 +1,21 @@
+# This file contains the solutions for the exercise in control__flows.py
+'''
+EXERCISE 1:
+Given that: letters = ['a', 'b', 'c']
+Write a list comprehension that returns: ['A', 'B', 'C']
+EXERCISE 2 (BONUS):
+Given that: word = 'abc'
+Write a list comprehension that returns: ['A', 'B', 'C']
+EXERCISE 3 (BONUS):
+Given that: fruits = ['Apple', 'Banana', 'Cherry']
+Write a list comprehension that returns: ['A', 'B', 'C']
+'''
+
+letters = ['a', 'b', 'c']
+[letter.upper() for letter in letters]  # iterate through a list of strings,
+                                        # and each string has an 'upper' method
+word = 'abc'
+[letter.upper() for letter in word]     # iterate through each character
+
+fruits = ['Apple', 'Banana', 'Cherry']
+[fruit[0] for fruit in fruits]          # slice the first character from each string
